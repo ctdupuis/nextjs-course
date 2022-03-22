66f894b645da8a94e0f643609cc27e4491e73c0f
Cody <relationalprogrammer@gmail.com>
2022-03-22 10:27:24 -0500

    mapped objects for list of links

--- pages/clients/index.jsx
+++ pages/clients/index.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function index() {
+
+  const clients = [
+    {id: "max", name: "Maximillian"},
+    {id: 'manuel', name: "Manuel"}
+  ]
+
   return (
     <div>
         <h1>List of Clients</h1>
+
+        <ul>
+          {clients.map(c => <li key={c.id}><Link href={`/clients/${c.id}`}>{c.name}</Link></li>)}
+        </ul>
     </div>
   )
 }
