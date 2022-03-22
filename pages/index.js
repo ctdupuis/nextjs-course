8819e2ebfb0e74c9650e60f78ba0ce9c03185927
Cody <relationalprogrammer@gmail.com>
2022-03-22 10:22:10 -0500

    started working with Next Link component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>Hello World</h1>
+
+      {/* This will send an entirely new req, losing app state in the process */}
+      <a href="/portfolio">Portfolio</a> 
+
+      {/* Proper NextJS Link, app state persists */}
+      <li>
+        <Link href="/portfolio">Portfolio</Link>
+      </li>
+
+      <li>
+        <Link href="/clients">Clients</Link>
+      </li>
     </div>
   )
 }
