c7f9e2cc8e8e12817b7bfa7f0124a824b78abdd1
Cody <relationalprogrammer@gmail.com>
2022-03-22 10:41:35 -0500

    programmatical navigation

--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -3,7 +3,19 @@ import { useRouter } from "next/router";
 export default function index() {
     const router = useRouter();
 
+    const loadHandler = () => {
+        // load data...
+        // router.push('/clients/max/projecta'); // simulates using a Link programmatically
+        router.replace({
+            pathname: '/clients/[id]/[projectId]',
+            query: { id: 'max', projectId: 'project_a'}
+        });
+    }
+
     return (
-        <div>All projects for a selected client with ID of {router.query.id}</div>
+        <div>
+            <h1>All projects for a selected client with ID of {router.query.id}</h1>
+            <button onClick={loadHandler}>Load Project</button>
+        </div>
     )
 }
